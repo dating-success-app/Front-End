15e27f612b4ebe8d7515d97163c3caf3b6445613
Landry Irakoze <irakozel@gmail.com>
2019-08-01 14:09:46 -0400

    dynamic routing registration

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -6,7 +6,7 @@ import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 
 
-const RegistrationForm = () => {
+const RegistrationForm = (props) => {
 
     const [userInfo, setUserInfo] = useState({
         username: "",
@@ -23,6 +23,7 @@ const RegistrationForm = () => {
         axios.post('https://dating-success.herokuapp.com/api/auth/register', userInfo)
             .then(response => { 
                 console.log(response.data)
+                props.history.push('/signin');
             })
             .catch(error => {
                 console.error(error)
